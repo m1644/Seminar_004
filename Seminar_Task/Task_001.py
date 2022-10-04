ccd5a92341767ec74bdc6bd6348644b4b3e23587
MaxX <33282787+m1644@users.noreply.github.com>
2022-10-04 08:12:28 +0300

    Add Task_001

--- Seminar_Task/Task_001.py
+++ Seminar_Task/Task_001.py
@@ -3,5 +3,16 @@
 которая покажет большее и меньшее число. В качестве символа-разделителя используйте пробел.
 '''
 
-a = 4
-print(a)
+list = []
+s = '1 15 16 4 24 8 44 7'
+list = s.split()
+print(list)
+
+max = int(list[0])
+min = int(list[0])
+for i in range(len(list)):
+    if int(list[i]) > max:
+        max = int(list[i])
+    elif int(list[i]) < min:
+        min = int(list[i])
+print(f'Максимальное число = {max}, минимальное = {min}')
